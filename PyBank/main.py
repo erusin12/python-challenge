8797ba05de72b69fb5970aebc5651f77a73d3ca2
erusin12 <127699499+erusin12@users.noreply.github.com>
2023-05-03 21:52:46 -0400

    Challenge 3 homework

---
+++ PyBank/main.py
@@ -0,0 +1,85 @@
+import os
+import csv
+
+budget_data_csv = os.path.join("Resources", "budget_data.csv")
+
+prev_value = None
+total_change = 0
+num_changes = 0
+output_file = os.path.join("analysis", "analysis.txt")
+    
+with open(budget_data_csv, 'r') as csvfile, open(output_file, 'w') as outfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    outfile.write("Financial Analysis\n")
+    outfile.write("---------------------------------\n")
+   # Skip the header row
+    next(csvreader)
+    # Create a set to store unique months
+    months = set()
+    # Iterate over each row in the CSV file
+    for row in csvreader:
+        # Extract the month value from the current row
+        month = row[0]
+        # Add the month value to the set
+        months.add(month)
+    # Count the number of unique months
+    num_months = len(months)
+    outfile.write(f"Total Months: {num_months}\n")
+
+    # Re-open the CSV file to iterate over it again
+    csvfile.seek(0)
+    next(csvreader) # skip header row
+
+    # Initialize variables to store the previous value and the total change
+    prev_value = None
+    total = 0 # add a variable to store the total
+    total_change = 0
+    num_changes = 0
+    max_increase = None
+    max_decrease = None
+    max_decrease_row = None
+    max_increase_row = None
+
+    # Iterate over each row in the CSV file
+    for row in csvreader:
+        # Extract the value from the current row and convert it to an integer
+        value = int(row[1])
+        total += value # accumulate the total
+
+        # If this is not the first row, calculate the difference from the previous row
+        if prev_value is not None:
+            change = value - prev_value
+            total_change += change
+            num_changes += 1
+            
+            # Check if this is the new maximum increase, and update the corresponding row
+            if max_increase is None or change > max_increase:
+                max_increase = change
+                max_increase_row = row
+
+            # Check if this is the new maximum decrease, and update the corresponding row
+            if max_decrease is None or change < max_decrease:
+                max_decrease = change
+                max_decrease_row = row
+
+        # Set the current value as the previous value for the next iteration
+        prev_value = value
+
+    # Write the total to the output file
+    outfile.write(f"Total: ${total}\n")
+
+    # Calculate the average change, or print a message if there were no changes
+    if num_changes > 0:
+        avg_change = total_change / num_changes
+        outfile.write(f"Average Change: ${avg_change:.2f}\n")
+    else:
+        outfile.write("There were no changes in the column.\n")
+    if max_increase_row is not None:
+        outfile.write(f"Greatest Increase in Profits: {max_increase_row[0]} (${max_increase:.0f})\n")
+    else:
+        outfile.write("There were no changes in the column.\n")
+    if max_decrease_row is not None:
+        outfile.write(f"Greatest Decrease in Profits: {max_decrease_row[0]} (${max_decrease:.0f})\n")
+    else:
+        outfile.write("There were no changes in the column.\n")
+        
---
+++ PyPoll/main.py
@@ -0,0 +1,41 @@
+import os
+import csv
+
+election_data_csv = os.path.join("Resources", "election_data.csv")
+
+output_file = os.path.join("analysis", "analysis.txt")
+    
+with open(election_data_csv, 'r') as csvfile, open(output_file, 'w') as outfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    print("Election Results\n", file=outfile)
+    print("------------------------------\n", file=outfile)
+    # Skip the header row
+    next(csvreader)
+    vote_counts = {}
+    # Iterate over each row in the CSV file
+    for row in csvreader:
+        # Extract the candidate name from the current row
+        candidate = row[2]
+        # Increment the vote count for the candidate in the dictionary
+        if candidate in vote_counts:
+            vote_counts[candidate] += 1
+        else:
+            vote_counts[candidate] = 1
+    # Count the total number of votes
+    total_votes = sum(vote_counts.values())
+    outfile.write(f"Total Votes: {total_votes}\n")
+    outfile.write("------------------------------\n")
+    # Iterate over the candidates in the vote counts
+    for candidate, count in vote_counts.items():
+        # Calculate the percentage of votes for the candidate
+        percentage = count / total_votes * 100
+        # Write the candidate name, vote count, and percentage to the output file
+        outfile.write(f"{candidate}: {percentage:.3f}% ({count})\n")
+  
+       
+    outfile.write("------------------------------\n")
+        # Find the candidate with the highest vote count
+    winner = max(vote_counts, key=vote_counts.get)
+    outfile.write(f"Winner: {winner}\n")
+    
+    outfile.write("------------------------------\n")
